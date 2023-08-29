0dfa078a1f7e180c2cb506b475171c4c5855dc0d
Joao Victor Raulino <116692477+joaoraulino@users.noreply.github.com>
2023-08-29 18:39:03 +0000

    Criação da funcionalidade de tag a para mostrat tudo, ativos e completos. Criação de funcionalidade para botão enter adicionar tarefas.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const inputBox = document.getElementById("input-box")
 const listContainer = document.getElementById("list-container")
+const allTasks = document.querySelectorAll("#list-container li")
+const activeTasks = document.querySelectorAll("#list-container li:not(.checked)")
+const completedTasks = document.querySelectorAll("list-container li.checked")
+const clearCompletes = document.getElementById("LimparCompletas")
 
 function addTask(){
     if(inputBox.value === ''){
@@ -17,6 +21,12 @@ function addTask(){
     saveData();
 }
 
+inputBox.addEventListener('keypress', function(e){
+    if(e.which == 13){
+       addTask();
+    }
+ }, false);
+
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
@@ -31,7 +41,44 @@ function saveData(){
     localStorage.setItem('data', listContainer.innerHTML)
 }
 
-function showTask(){
+
+
+function showAll(){
     listContainer.innerHTML = localStorage.getItem("data");
 }
-showTask();
+showAll();
+
+const tasks = document.querySelectorAll("#list-container li");
+
+document.querySelector(".content--footer").addEventListener("click", function(event) {
+    if (event.target.tagName === "A") {
+        event.preventDefault();
+
+        const filter = event.target.getAttribute("data-filter");
+
+        document.querySelectorAll(".content--footer a").forEach(link => {
+            link.classList.remove("selected");
+        });
+
+        event.target.classList.add("selected");
+
+        tasks.forEach(task => {
+            if (filter === "all") {
+                task.style.display = "block";
+            } else if (filter === "active") {
+                task.style.display = task.classList.contains("checked") ? "none" : "block";
+            } else if (filter === "completed") {
+                task.style.display = task.classList.contains("checked") ? "block" : "none";
+            }
+        });
+    }
+});
+
+document.getElementById("LimparCompletas").addEventListener("click", function(event) {
+    event.preventDefault();
+    tasks.forEach(task => {
+        if (task.classList.contains("checked")) {
+            task.remove();
+        }
+    });
+});
